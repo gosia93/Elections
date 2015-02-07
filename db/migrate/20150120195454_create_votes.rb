e169f500ac7a36bc45fbce1e8845e1d40664ec57
Gosia <gosia.koselak@gmail.com>
2015-02-07 12:26:11 +0100

    active_admin

--- db/migrate/20150120195454_create_votes.rb
+++ db/migrate/20150120195454_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :committee
-      t.integer :constituency
+      t.integer :cmmiettee_id
+      t.integer :constituency_id
       t.integer :amount
 
       t.timestamps null: false
