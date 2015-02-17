d78b3dd53ba9760d8cc021d179507c2521f86741
Gosia <gosia.koselak@gmail.com>
2015-02-17 01:43:48 +0100

    sum

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,23 @@
 Rails.application.routes.draw do
-
-
- # devise_for :admin_users, ActiveAdmin::Devise.config
- # ActiveAdmin.routes(self) 
- # resources :results
- # resources :committees
- # resources :constituencies
- # resources :users
- # resources :roles
  
- devise_for :users
- scope "/admin" do
-  resources :users
+  devise_for :users
+  scope "/admin" do
+    resources :users
   end
 
   resources :users
   resources :roles
- resources :committees
- resources :constituencies
- resources :results do
-  member do
-    get 'sum'
+  resources :committees
+  resources :constituencies
+  resources :results 
+ # get 'sum' => 'results#sum', :name=> 'suma'
+  
+   authenticated :user do
+    root :to => 'results#sum', as: :authenticated_root
   end
- end 
+   
+    root :to => 'welcome#index'
 
- authenticated :user do
-     root :to => 'welcome#index_admin', as: :authenticated_root
-   end
-      root :to => 'welcome#index'
 
-  end
+end
 
